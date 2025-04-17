b97cb7da620f63f2fdbce2ee3948bf1ac309f94e
JJMaish <pea.jjprince@yahoo.com>
2025-04-17 17:45:32 +0200

    Delete Agents directory

--- Agents/index.js
+++
@@ -1,5 +0,0 @@
-export { default as EventsAgent } from './EventsAgent.js';
-export { default as NightlifeAgent } from './NightlifeAgent.js';
-export { default as AttractionsAgent } from './AttractionsAgent.js';
-export { default as PlannerAgent } from './PlannerAgent.js';
-export { default as ChatAgent } from './ChatAgent.js'; 
